cbcc5d34250a01262c2e7b87e80cc10a61be3d4b
Hello-hao <Hello-hao@users.noreply.github.com>
2026-06-15 09:33:22 +0800

    2026-06-15 代码优化更新

--- src/test/java/cn/hellohao/Tbed/TbedApplicationTests.java
+++ src/test/java/cn/hellohao/Tbed/TbedApplicationTests.java
@@ -2,15 +2,23 @@ package cn.hellohao.Tbed;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TbedApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     public void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
