1dbd7653acfcab983e2332bc111646a8c1787755
Hello-hao <Hello-hao@users.noreply.github.com>
2026-07-22 21:24:59 +0800

    2026-07-22 代码优化更新

--- src/test/java/cn/hellohao/Tbed/TbedApplicationTests.java
+++ src/test/java/cn/hellohao/Tbed/TbedApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,6 +20,7 @@ public class TbedApplicationTests {
     @Test
     public void contextLoads() {
         assertNotNull(applicationContext);
+        assertTrue(applicationContext.containsBean("tbedApplication"));
     }
 
 }
